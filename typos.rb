5a73ce4493034c58e9702f2a153d6fc7a0757457
Emmapr <emmapriester@hotmail.com>
2021-02-10 15:22:19 +0000

    load and edit students in other file

--- typos.rb
+++ typos.rb
@@ -4,14 +4,14 @@ def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
 # get the first name
-  name = gets.chomp
+  name = STDIN.gets
 # while the name is not empty, repeat this code
   while !name.empty? do
 # add the student hash to the array
   @students << {name: name, cohort: :november}
   puts "Now we have #{@students.count} students"
 # get another name from the user
-  name = gets.chomp
+  name = STDIN.gets
   end
 #return the array of students
   @students
@@ -50,13 +50,25 @@ def save_students
   file.close
 end
 
-def load_students
-  file = File.open("students.csv", "r")
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
   file.readlines.each { |line|
     name, cohort = line.chomp.split(",")
     @students << {name: name, cohort: cohort.to_sym}
   }
   file.close
+end
+
+def try_load_students
+  filename = ARGV.first
+  return if filename.nil?
+  if File.exists?(filename)
+    load_students(filename)
+    puts "loaded #{@students.count} from #{filename}"
+  else
+    puts "sorry, #{filename} doesnt exist"
+    exit
+  end
 end
 
 def show_students
@@ -68,7 +80,7 @@ end
 def process(selection)
   case selection
   when "1"
-    @students = input_students
+    @students = STDIN.gets
   when "2"
     show_students
   when "3"
